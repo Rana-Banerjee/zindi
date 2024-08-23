7c813cd9ebf691c7c9e3929c3a8925f29f895169
Rana Banerjee <rana1224@gmail.com>
2024-08-23 06:55:29 +0000

    updates

--- init_script2.sh
+++ init_script2.sh
@@ -4,5 +4,6 @@ cd transformers
 yes | pip install updgrade pip
 pip install -e .
 cd ..
+cp common/transformers.py ~/.cache/pypoetry/virtualenvs/zindi-LtLKIbXv-py3.9/lib/python3.9/site-packages/ctranslate2/converters/
 git config --global user.name "Rana Banerjee"
 git config --global user.email "rana1224@gmail.com"
