f30b465693e756e417d7247f8d5349ae2d77310f
Rana Banerjee <rana1224@gmail.com>
2024-08-26 07:04:07 +0000

    marian init

---
+++ marian_init.sh
@@ -0,0 +1,16 @@
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.6.0/local_installers/cuda-repo-ubuntu2204-12-6-local_12.6.0-560.28.03-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-6-local_12.6.0-560.28.03-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-6-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-6
+sudo apt-get install -y nvidia-open
+sudo apt-get install libprotobuf10 protobuf-compiler libprotobuf-dev
+rm -rf cuda-repo-ubuntu2204-12-6-local_12.6.0-560.28.03-1_amd64.deb
+git clone https://github.com/marian-nmt/marian
+cd marian
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DUSE_SENTENCEPIECE=ON
+make -j 8
